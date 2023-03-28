f6fe3c62acfe4b735464bf2fd5032a2253ab2633
Kyle M <ksmontville@gmail.com>
2023-03-28 13:03:52 -0400

    Token stuff

--- src/store/userData.js
+++ src/store/userData.js
@@ -2,8 +2,8 @@ import {useAuth0} from "@auth0/auth0-vue";
 import { defineStore } from 'pinia'
 
 export const useUserDataStore = defineStore('userData',  () => {
-  const { loginWithRedirect, logout, user, isAuthenticated } = useAuth0()
+  const { loginWithRedirect, logout, getAccessTokenSilently, user, isAuthenticated } = useAuth0()
 
-  return { loginWithRedirect, logout, user, isAuthenticated }
+  return { loginWithRedirect, logout, getAccessTokenSilently, user, isAuthenticated }
 
 })
--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -1,16 +1,40 @@
 <template>
 
 
-  <p v-if="userData.isAuthenticated">{{ userData.user.name }}'s Dashboard</p>
+  <p v-if="userData.isAuthenticated">{{ userData.user.name }}'s Dashboard
+
+
+      My Subscriptions
+      {{ subscriptions }}
+
+  </p>
 
   <p v-else>Please login or register to continue.</p>
 
 </template>
 
 <script setup>
+  import {ref} from "vue";
+  import axios from 'axios'
   import { useUserDataStore } from "@/store/userData";
-
+  import {useAuth0} from "@auth0/auth0-vue";
   const userData =  useUserDataStore()
+
+  const subscriptions = ref("")
+
+  const { getAccessTokenSilently } = useAuth0()
+  const getData = async () => {
+    const token = await getAccessTokenSilently();
+    const response = await axios.get("https://codingcatllc.us.auth0.com/api/v2/users/google-oauth2|106996953159648136381", {
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    })
+    const data = response.data
+    console.log(data)
+  }
+
+  getData()
 
 </script>
 
