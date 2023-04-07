c4bafe56e235f4f4feed9b18b420668d3f236379
Kyle Montville <ksmontville@gmail.com>
2023-04-07 12:40:00 -0400

    Begin logging known issues.

--- src/plugins/index.js
+++ src/plugins/index.js
@@ -32,6 +32,8 @@ export function registerPlugins (app) {
         {
           domain: import.meta.env.VITE_AUTH0_DOMAIN,
           clientId: import.meta.env.VITE_AUTH0_CLIENT_ID,
+          useRefreshTokens: true,
+          cacheLocation: 'localstorage',
           authorizationParams: {
             redirect_uri: window.location.origin
           }
