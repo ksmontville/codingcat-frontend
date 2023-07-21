92b225da110f799691fb2e1627c1cf525019ae07
Kyle Montville <ksmontville@gmail.com>
2023-07-21 10:42:19 -0700

    Clean up userDataStore.

--- src/components/StripeCustomerPortal.vue
+++ src/components/StripeCustomerPortal.vue
@@ -17,7 +17,6 @@ const props = defineProps(
 const userData = useUserDataStore()
 
 const createPortalSession = async () => {
-  // const token = await userData.getManagementToken()
   const response = await axios.post(`${import.meta.env.VITE_BACKEND_API_URL}/customer/${props.stripeCustomerId}/create-customer-portal-session`,
     {
       customer_id: props.stripeCustomerId
