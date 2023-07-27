05dea744b99061cfa12b14d5477fff088fdcdad2
Kyle Montville <ksmontville@gmail.com>
2023-07-27 08:28:59 -0700

    Change token call from POST to GET.

--- src/components/StripeCustomerPortal.vue
+++ src/components/StripeCustomerPortal.vue
@@ -17,10 +17,10 @@ const props = defineProps(
 const userData = useUserDataStore()
 
 const createPortalSession = async () => {
-  const response = await axios.post(`${import.meta.env.VITE_BACKEND_API_URL}/customer/${props.stripeCustomerId}/create-customer-portal-session`,
-    {
-      customer_id: props.stripeCustomerId
-    },
+  const response = await axios.get(`${import.meta.env.VITE_BACKEND_API_URL}/customer/${props.stripeCustomerId}/create-customer-portal-session`,
+    // {
+    //   customer_id: props.stripeCustomerId
+    // },
     {
       headers: {
         Authorization: `Bearer ${userData.managementToken}`
